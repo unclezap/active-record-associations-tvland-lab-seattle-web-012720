e3d406d15ab51dad56a8eb7e5695bdbfdde15ac2
unclezap <zeb.blackwell@gmail.com>
2020-02-11 01:56:04 -0800

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    self.first_name
+    self.last_name
+    return "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    # binding.pry
+    return "#{self.characters.map {|c| c.name}[0]} - #{self.shows.map {|s| s.name}[0]}"
+  end
+
 end
